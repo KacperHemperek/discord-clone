def7485258b7b83939d28649fb52d74e35206a0e
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-03-16 10:23:23 +0100

    refactor(server/store): add scanner interface, so we can pass rows and row to scanners

--- server/store/utils.go
+++ server/store/utils.go
@@ -1,11 +1,17 @@
 package store
 
 import (
-	"database/sql"
 	"github.com/kacperhemperek/discord-go/models"
 )
 
-func scanUser(rows *sql.Rows) (*models.User, error) {
+// Scanner is a helper interface that wraps the Scan method implemented by sql.Rows and sql.Row
+type Scanner interface {
+	Scan(dest ...any) error
+}
+
+// Scans user from the given Scanner, this function accepts sql.Rows and sql.Row as Scanner
+// and returns a user or an error if the scan fails
+func scanUser(rows Scanner) (*models.User, error) {
 	user := &models.User{}
 
 	err := rows.Scan(
@@ -23,4 +29,27 @@ func scanUser(rows *sql.Rows) (*models.User, error) {
 	}
 
 	return user, nil
+}
+
+// Scans friendship from the given Scanner, this function accepts sql.Rows and sql.Row as Scanner
+// and returns a friendship or an error if the scan fails
+func scanFriendship(row Scanner) (*models.Friendship, error) {
+	friendship := &models.Friendship{}
+
+	err := row.Scan(
+		&friendship.ID,
+		&friendship.InviterID,
+		&friendship.FriendID,
+		&friendship.Status,
+		&friendship.Accepted,
+		&friendship.Seen,
+		&friendship.RequestedAt,
+		&friendship.StatusChangedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return friendship, nil
 }
