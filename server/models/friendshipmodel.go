0e26273a6dcf038db6dfb3d4ebb2cf7fbfa9f979
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-03-16 10:22:02 +0100

    fix(server/models): add missing fields to friendship model

--- server/models/friendshipmodel.go
+++ server/models/friendshipmodel.go
@@ -2,10 +2,11 @@ package models
 
 type Friendship struct {
 	ID              int    `json:"id"`
-	UserID          int    `json:"userId"`
+	InviterID       int    `json:"inviterId"`
 	FriendID        int    `json:"friendId"`
 	Accepted        bool   `json:"accepted"`
 	Seen            bool   `json:"seen"`
+	Status          string `json:"status"`
 	RequestedAt     string `json:"requestedAt"`
 	StatusChangedAt string `json:"statusChangedAt"`
 }
