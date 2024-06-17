f7e3314d7822aaf27efc61b9fbda49d25d0a8674
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-06-17 22:46:28 +0200

    feat(client/friends): opacity transition on friend item buttons

--- client/src/app/components/friends/FriendItemButton.tsx
+++ client/src/app/components/friends/FriendItemButton.tsx
@@ -15,7 +15,7 @@ const FriendListItemButton = React.forwardRef<
       ref={ref}
       onClick={onClick}
       disabled={disabled}
-      className="p-2 rounded-full bg-dc-neutral-950 group disabled:opacity-50 disabled:cursor-not-allowed transition-opacity duration-100"
+      className="p-2 rounded-full bg-dc-neutral-950 group disabled:opacity-50 disabled:cursor-not-allowed transition-opacity duration-75 hover:opacity-75"
     >
       {icon}
     </button>
